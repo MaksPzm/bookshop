78189a9970a522760ff039ac7b05a809b9145d68
MaksPzm <makspzm@gmail.com>
2025-06-17 01:50:40 +0500

    добавлено сохранение в localStorage

--- script/script.ts
+++ script/script.ts
@@ -32,7 +32,7 @@ class Book {
     rating;
     description;
     price;
-    constructor(img, after, name, rating, description, price) {
+    constructor(img: string, after: string, name: string, rating, description, price) {
         this.img = img;
         this.after = after;
         this.name = name;
@@ -99,6 +99,9 @@ function loadCategoriesActive() {
     categories[localIndexActive].classList.add("containner__categories-list-item_active");
     categoriesInput[localIndexActive].classList.add("containner__categories-list-item-submit_active");
 }
+let boocksData: any = [ ];
+let boocksDataLocal: any = localStorage.getItem("publishedBooks") ? JSON.parse(localStorage.getItem("publishedBooks")) : [ ];
+console.log("b.D", boocksDataLocal);
 
 
 
@@ -107,6 +110,7 @@ function API() {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${category}&key=AIzaSyCvzFkuSjiZgJOh90lZaYuv9qUT2xypKNs&printType=books&startIndex=0&maxResults=6&langRestrict=en`)
         .then((response) => response.json())
         .then(displayResult)
+        .then(dataBook)
         .catch((err) => console.log("ERROR"))
         
 }
@@ -119,8 +123,9 @@ function displayResult(data: any) {
     for (let i = 0; i < resultAPI.length; i++) {
         let price: number | string = resultAPI[i].saleInfo.retailPrice ? "$" + (resultAPI[i].saleInfo.retailPrice.amount / 80).toFixed(2) : "free";
         console.log("pr", price);
-        let averageRating: string = resultAPI[i].volumeInfo.averageRating ? resultAPI[i].volumeInfo.averageRating : "The rating is not";
-        let ratingsCount: string = resultAPI[i].volumeInfo.ratingsCount ? resultAPI[i].volumeInfo.ratingsCount + " review" : " ";
+        let averageRating: string = resultAPI[i].volumeInfo.averageRating ? resultAPI[i].volumeInfo.averageRating : "";
+        let ratingsCount: string = resultAPI[i].volumeInfo.ratingsCount ? resultAPI[i].volumeInfo.ratingsCount + " review" : "";
+        let description: string = resultAPI[i].volumeInfo.description ? resultAPI[i].volumeInfo.description : "There is no description";
         const showBook = `
             <div class="container__books-block" index=${i} id="book-${i}">
                 <img class="container__books-block-img" src="${resultAPI[i].volumeInfo.imageLinks.thumbnail}">
@@ -131,12 +136,68 @@ function displayResult(data: any) {
                         <div class="container__books-block-info-rating-averageRating">${averageRating}</div>
                         <span class="container__books-block-info-rating-ratingsCount">${ratingsCount}</span>
                     </div>
-                    <p class="container__books-block-info-description">${resultAPI[i].volumeInfo.description}</p>
+                    <p class="container__books-block-info-description">${description}</p>
                     <span class="container__books-block-info-price">${price}</span>
+                    <button type="button" class="container__books-block-info-btn">buy now</button>
                 </div>
             </div>
         `
         containerBooks?.insertAdjacentHTML("beforeend", showBook)
+        
     }
     
 }
+
+
+function dataBook(): void {
+    const btnBuy = document.querySelectorAll(".container__books-block-info-btn");
+    console.log("btnBuy", btnBuy);
+    
+    btnBuy.forEach((bay, index, array) => {
+        bay.addEventListener("click", () => {
+            const boock = bay.closest(`#book-${index}`);
+            if (boock == null) return;
+            const indexBoock: string | null = boock.getAttribute("index")
+            const imgBock: HTMLImageElement | null = boock?.querySelector(".container__books-block-img") ? boock.querySelector(".container__books-block-img") : null;
+            let srcImgBock: string = ""
+            if (imgBock !== null) srcImgBock = imgBock.src;
+            const autorBock: string = <string>boock?.querySelector(".container__books-block-info-autor")?.textContent;
+            const titleBock: string = <string>boock.querySelector(".container__books-block-info-title")?.textContent;
+            const averageRatingBock: string | null = boock.querySelector(".container__books-block-info-rating-averageRating") ? <string>boock.querySelector(".container__books-block-info-rating-averageRating")?.textContent : null;
+            const ratingCountBock: string | null = boock.querySelector(".container__books-block-info-rating-ratingsCount") ? <string>boock.querySelector(".container__books-block-info-rating-ratingsCount")?.textContent : null;
+            const descriptionBock: string | null = boock.querySelector(".container__books-block-info-description") ? <string>boock.querySelector(".container__books-block-info-description")?.textContent : null;
+            const priceBock: string | null = boock.querySelector(".container__books-block-info-price") ? <string>boock.querySelector(".container__books-block-info-price")?.textContent : null;
+            type infoBock = {
+                indexBoock: string | null,
+                srcImgBock: string,
+                autorBock: string,
+                titleBock: string,
+                averageRatingBock: string | null,
+                ratingCountBock: string | null,
+                descriptionBock: string | null,
+                priceBock: string | null,
+        };
+            let infoBock: infoBock = {
+                indexBoock: indexBoock,
+                srcImgBock: srcImgBock,
+                autorBock: autorBock,
+                titleBock: titleBock,
+                averageRatingBock: averageRatingBock,
+                ratingCountBock: ratingCountBock,
+                descriptionBock: descriptionBock,
+                priceBock: priceBock
+            }
+            boocksData.push(infoBock);
+            boocksDataLocal.push(infoBock);
+            localStorage.setItem("publishedBooks", JSON.stringify(boocksDataLocal));
+        })
+    })
+}
+
+
+const addToBook = (() => {
+    const cart: HTMLDivElement | null = document.querySelector(".header__user-block-cart");
+    if (cart === null) return;
+
+
+})()
