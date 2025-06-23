9370d8dbe97c416c5c665c5ba7e75bc44262b2ec
MaksPzm <makspzm@gmail.com>
2025-06-24 00:25:31 +0500

    изменение путей картинок

--- script/script.ts
+++ script/script.ts
@@ -17,7 +17,7 @@ menuActive()
 function slider(): void {
     const dots = [...document.querySelectorAll(".banner__slider-dots-dot")];
     const blockBanner: HTMLDivElement = <HTMLDivElement>document.querySelector(".baner__img");
-    const img = ["./images/png/banner1.png", "./images/png/banner2.png", "./images/png/banner3.png"];
+    const img = ["images/png/banner1.png", "images/png/banner2.png", "images/png/banner3.png"];
     const newSlider = new Sliders(dots, blockBanner, img);
     newSlider.slider()
     let indexDots = 0;
